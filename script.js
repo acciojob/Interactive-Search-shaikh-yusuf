ccdf9f9e01c5053755ebd0a306bdf98942adb3c2
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-11-18 10:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+            const searchDiv = document.querySelector('.search');
+            const input = document.querySelector('.input');
+            const btn = document.querySelector('.btn');
+
+            btn.addEventListener('click', function () {
+                searchDiv.classList.add('active');
+                input.focus();
+            });
+        });
